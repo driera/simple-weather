b8b8244f04d00d50599f1f6d8855f6056c1e26a3
Dani Riera <daniel.riera@qatium.com>
2021-01-24 18:45:59 +0100

    Delete moved tests

--- src/components/FiveDaysWeather/__tests__/FiveDaysWeather.spec.js
+++ src/components/FiveDaysWeather/__tests__/FiveDaysWeather.spec.js
@@ -1,64 +1,64 @@
-import { shallowMount } from "@vue/test-utils";
-import FiveDaysWeather from "../FiveDaysWeather";
+import {shallowMount} from '@vue/test-utils';
+import FiveDaysWeather from '../FiveDaysWeather';
 
-describe("FiveDaysWeather", () => {
-  let props;
-  let weatherData = {
-    "2021-01-18": [
-      {
-        pressure: 1028,
-        temperature: 14.94,
-        temperatureMin: 14.94,
-        temperatureMax: 15.96,
-        state: "Clouds",
-        stateDescription: "nubes",
-        windSpeed: 6.73,
-        windAngle: 264,
-        time: "12:00:00",
-        id: 1579348800,
-      },
-    ],
-  };
-
-  beforeEach(() => {
-    props = {
-      weatherData,
+describe('FiveDaysWeather', () => {
+    let props;
+    let weatherData = {
+        '2021-01-18': [
+            {
+                pressure: 1028,
+                temperature: 14.94,
+                temperatureMin: 14.94,
+                temperatureMax: 15.96,
+                state: 'Clouds',
+                stateDescription: 'nubes',
+                windSpeed: 6.73,
+                windAngle: 264,
+                time: '12:00:00',
+                id: 1579348800
+            }
+        ]
     };
-  });
 
-  const build = () => {
-    const wrapper = shallowMount(FiveDaysWeather, {
-      propsData: props,
+    beforeEach(() => {
+        props = {
+            weatherData
+        };
     });
 
-    return {
-      wrapper,
+    const build = () => {
+        const wrapper = shallowMount(FiveDaysWeather, {
+            propsData: props
+        });
+
+        return {
+            wrapper
+        };
     };
-  };
 
-  it("renders the component", () => {
-    const { wrapper } = build();
-    expect(wrapper).toMatchSnapshot();
-  });
+    it('renders the component', () => {
+        const {wrapper} = build();
+        expect(wrapper).toMatchSnapshot();
+    });
 
-  it("renders correct time", () => {
-    const time = "12:00:00";
+    it('renders correct time', () => {
+        const time = '12:00:00';
 
-    const { wrapper } = build();
+        const {wrapper} = build();
 
-    expect(wrapper.find("span").text()).toBe(time);
-  });
+        expect(wrapper.find('span').text()).toBe(time);
+    });
 
-  it("renders correct state", () => {
-    const state = "Clouds";
+    it('renders correct state', () => {
+        const state = 'Clouds';
 
-    const { wrapper } = build();
+        const {wrapper} = build();
 
-    expect(
-      wrapper
-        .findAll("span")
-        .at(1)
-        .text()
-    ).toBe(state);
-  });
+        expect(
+            wrapper
+                .findAll('span')
+                .at(1)
+                .text()
+        ).toBe(state);
+    });
 });
