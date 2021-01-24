6490953a2976edb1dbdebc9bf0dc30ea77ca7bde
Dani Riera <daniel.riera@qatium.com>
2021-01-24 18:45:25 +0100

    Refactor CurrentWeather

--- src/components/CurrentWeather/CurrentWeather.vue
+++ src/components/CurrentWeather/CurrentWeather.vue
@@ -1,106 +1,90 @@
 <template>
-    <div v-if="weatherData" class="current">
-        <div class="container">
-            <h1>kjadsn</h1>
-            <div class="current-main">
-                <div class="current-main--location">{{weatherData.city}}</div>
-                <div class="current-main--time">{{weatherData.hour}} — {{weatherData.date}}</div>
-                <div class="current-main--temp">
-                    <span class="current-main--temp-text">{{Math.round(weatherData.temperature)}}</span>
-                    <span class="current-main--temp-symbol">º</span>
-                </div>
-                <div class="current-main--conditions">
-                    <component :is="weatherData.stateIcon" class="current-main--conditions-icon"></component>
-                    <div class="current-main--conditions-text">{{weatherData.stateDescription}}</div>
-                </div>
-            </div>
-            <div class="current-details">
-                <div class="current-details--title">
-                    <span>Detalles</span>
-                </div>
-                <div class="current-details--item">
-                    <div class="current-details--content">
-                        <div class="current-details--subtitle">Viento</div>
-                        <span class="current-details--sub">{{weatherData.windAngle}}</span>
-                        {{weatherData.windSpeed}}
-                        <span class="current-details--units">km/h</span>
-                    </div>
-                    <div class="current-details--icon">
-                        <icon-wind class="current-details--icon"></icon-wind>
-                    </div>
-                </div>
-                <div class="current-details--item">
-                    <div class="current-details--content">
-                        <div class="current-details--subtitle">Nubosidad</div>
-                        {{weatherData.clouds}}
-                        <span class="current-details--units">%</span>
-                    </div>
-                    <div class="current-details--icon">
-                        <icon-cloud></icon-cloud>
-                    </div>
-                </div>
-                <div class="current-details--item">
-                    <div class="current-details--content">
-                        <div class="current-details--subtitle">Humedad</div>
-                        {{weatherData.humidity}}
-                        <span class="current-details--units">%</span>
-                    </div>
-                    <div class="current-details--icon">
-                        <icon-raindrop></icon-raindrop>
-                    </div>
-                </div>
-                <div class="current-details--item">
-                    <div class="current-details--content">
-                        <div class="current-details--subtitle">Presión</div>
-                        {{weatherData.pressure}}
-                        <span class="current-details--units">mBar</span>
-                    </div>
-                    <div class="current-details--icon">
-                        <icon-pressure></icon-pressure>
-                    </div>
-                </div>
-            </div>
-            <div class="current-details">
-                <div class="current-details--title">
-                    <span>Salida/Puesta de sol</span>
-                </div>
-                <div class="current-details--item">
-                    <div class="current-details--content">
-                        <div class="current-details--subtitle">Salida sol</div>
-                        {{weatherData.sunrise}}
-                    </div>
-                    <div class="current-details--icon">
-                        <icon-sunrise></icon-sunrise>
-                    </div>
-                </div>
-                <div class="current-details--item">
-                    <div class="current-details--content">
-                        <div class="current-details--subtitle">Puesta sol</div>
-                        {{weatherData.sunset}}
-                    </div>
-                    <div class="current-details--icon">
-                        <icon-sunset></icon-sunset>
-                    </div>
-                </div>
-            </div>
-            <div class="current-log">{{ weatherData }}</div>
+  <div v-if="weatherData" class="current">
+    <div class="container">
+      <h1>Tiempo hoy</h1>
+      <div class="intro">
+        <div class="location">{{ weatherData.city }}</div>
+        <div class="time">
+          {{ weatherData.hour }} — {{ weatherData.date }}
         </div>
+
+        <div class="temp">
+          <span>{{ Math.round(weatherData.temperature) }}</span>
+          <span class="temp-symbol">º</span>
+        </div>
+
+        <div class="conditions">
+          <component
+            :is="weatherData.stateIcon"
+            class="conditions-icon"
+          />
+          <span>{{ weatherData.stateDescription }}</span>
+        </div>
+      </div>
+      <div class="details">
+        <div class="details--title">
+          <span>Detalles</span>
+        </div>
+        <DetailsItem
+          title="Viento"
+          :content="weatherData.windSpeed"
+          :sub="weatherData.windAngle"
+          units="km/h"
+          icon="icon-wind"
+        />
+        <DetailsItem
+          title="Nubosidad"
+          :content="weatherData.clouds"
+          units="%"
+          icon="icon-cloud"
+        />
+        <DetailsItem
+          title="Humedad"
+          :content="weatherData.humidity"
+          units="%"
+          icon="icon-raindrop"
+        />
+        <DetailsItem
+          title="Presión"
+          :content="weatherData.pressure"
+          units="mBar"
+          icon="icon-pressure"
+        />
+      </div>
+      <div class="details">
+        <div class="details--title">
+          <span>Salida/Puesta de sol</span>
+        </div>
+        <DetailsItem
+          title="Salida sol"
+          :content="weatherData.sunrise"
+          icon="icon-sunrise"
+        />
+        <DetailsItem
+          title="Puesta sol"
+          :content="weatherData.sunset"
+          icon="icon-sunset"
+        />
+      </div>
+      <div class="log">{{ weatherData }}</div>
     </div>
+  </div>
 </template>
 
 <script>
+import DetailsItem from "@/components/DetailsItem";
 export default {
-    name: 'CurrentWeather',
-    props: {
-        weatherData: {
-            type: Object
-        }
-    }
-
+  name: "CurrentWeather",
+  components: { DetailsItem },
+  props: {
+    weatherData: {
+      type: Object,
+    },
+  },
 };
 </script>
 
 <style scoped>
-    @import '../../assets/styles/helpers/variables';
-    @import './CurrentWeather';
+@import "../../assets/styles/helpers/variables";
+@import "./CurrentWeather";
 </style>
---
+++ src/components/DetailsItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="item">
+    <div class="content">
+      <div class="title">{{ title }}</div>
+      <span class="sub" v-if="sub">{{ sub }}</span>
+      {{ content }}
+      <span class="units" v-if="units">{{ units }}</span>
+    </div>
+    <div class="icon">
+      <component :is="icon" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailsItem",
+  props: {
+    title: String,
+    sub: String,
+    content: String,
+    units: String,
+    icon: String,
+  },
+};
+</script>
+
+<style scoped>
+@import "../assets/styles/helpers/variables";
+
+.item {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: center;
+  padding: 20px 10px 5px;
+  background: color-mod(var(--light-color) a(5%));
+  border-radius: 3px;
+}
+
+.content {
+  position: relative;
+  font-size: 18px;
+  font-weight: 800;
+}
+
+.title {
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  font-size: 12px;
+  font-weight: 300;
+}
+
+.units {
+  font-size: 12px;
+}
+
+.sub {
+  font-size: 13px;
+}
+
+.icon {
+  font-size: 40px;
+}
+</style>
