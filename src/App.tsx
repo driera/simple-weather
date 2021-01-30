ace987b28d544b60bfae231ed70665ff68c932d0
Dani Riera <daniel.riera@qatium.com>
2021-01-31 00:41:53 +0100

    Add theme styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { FunctionComponent } from "react";
+import { Current } from "./components/Current";
+
+import "./theme/reset.css";
+import "./theme/variables.css";
+import "./theme/default.css";
 
 const App: FunctionComponent = () => {
-  return <div>Hola</div>;
+  return <Current />;
 };
 
 export default App;
