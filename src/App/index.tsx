84c9610e5faba907f373e61fe8633dfad44a8d3c
Dani Riera <daniel.riera@qatium.com>
2021-02-05 17:28:41 +0100

    Rethink folder structure

---
+++ src/App/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+import { Current } from "./Current";
+
+const App: FunctionComponent = () => {
+  return <Current />;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "./theme/reset.css";
+import "./theme/variables.css";
+import "./theme/default.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
