14d64caded4b7c39866821dc12a3864247068ba2
Dani Riera <daniel.riera@qatium.com>
2024-12-29 19:36:14 +0100

    Update deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
 import "./theme/reset.css";
 import "./theme/variables.css";
 import "./theme/default.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
