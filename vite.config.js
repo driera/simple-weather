df9bc43943d91fdf4e88effd0efe631ec6af5e1e
Dani Riera <daniel.riera@qatium.com>
2024-12-30 19:02:00 +0100

    Add support for CSS modules

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import postcssPresetEnv from "postcss-preset-env";
+import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin()]
+  plugins: [react(), svgrPlugin()],
+  css: {
+    modules: {
+      localsConvention: "camelCase"
+    },
+    postcss: {
+      plugins: [
+        postcssPresetEnv({
+          stage: 2,
+          features: {
+            "nesting-rules": true
+          }
+        })
+      ]
+    }
+  }
 });
