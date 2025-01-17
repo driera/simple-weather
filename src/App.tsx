99bafd6549100e0931819de4e9d418f6cc134fcd
Dani Riera <daniel.riera@qatium.com>
2025-01-17 20:09:25 +0100

    Show five days forecast in main view

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,28 @@ import { CurrentWeather } from "./domain/current-weather-entity";
 import { WeatherApiClient } from "./repositories/weather-api-client";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { useGeoLocation } from "./useGeoLocation";
+import styles from "./App.module.css";
+import { FiveDaysForecast, Forecast } from "./domain/five-days-forecast-entity";
 
 const App: FunctionComponent = () => {
   const [weatherData, setWeatherData] = useState<CurrentWeather | null>(null);
+  const [fiveDaysData, setFiveDaysData] = useState<FiveDaysForecast | null>(
+    null
+  );
   const coordinates = useGeoLocation();
 
   useEffect(() => {
     const fetchData = async () => {
       if (coordinates) {
         const weather = new WeatherApiClient().setCoordinates(coordinates);
-        const result = await weather.getCurrentWeather();
-        if (result.status === "success") {
-          setWeatherData(result.data);
+        const currentWeather = await weather.getCurrentWeather();
+        if (currentWeather.status === "success") {
+          setWeatherData(currentWeather.data);
+        }
+
+        const fiveDays = await weather.getFiveDaysWeather();
+        if (fiveDays.status === "success") {
+          setFiveDaysData(fiveDays.data);
         }
       }
     };
@@ -25,7 +35,31 @@ const App: FunctionComponent = () => {
 
   return (
     <ErrorBoundary>
-      <CurrentView weatherData={weatherData} />;
+      <section className={styles.weather}>
+        <div className={styles.sun}></div>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Hoy</h1>
+        </div>
+        <CurrentView weatherData={weatherData} />
+        <div className={styles.fiveDays}>
+          <h2 className={styles.title}>Próximas horas</h2>
+          {fiveDaysData && (
+            <div className={styles.fiveDaysList}>
+              {fiveDaysData.list.map((item: Forecast) => (
+                <div className={styles.listItem} key={item.date}>
+                  <div className={styles.itemDate}>
+                    {new Date(item.date * 1000).toLocaleString("es-ES", {
+                      dateStyle: "short",
+                      timeStyle: "short"
+                    })}
+                  </div>
+                  <div className={styles.temperature}>{item.temperature}º</div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
     </ErrorBoundary>
   );
 };
--- src/views/Current/CurrentView.tsx
+++ src/views/Current/CurrentView.tsx
@@ -8,68 +8,64 @@ import { CurrentWeather } from "../../domain/current-weather-entity";
 export const CurrentView: FunctionComponent<{
   weatherData: CurrentWeather | null;
 }> = ({ weatherData }) => {
-  if (!weatherData) return <div>loading...</div>;
+  if (!weatherData) return <div style={{ color: "#000" }}>loading...</div>;
 
   return (
     <section className={styles.current}>
-      <div className={styles.sun}></div>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Hoy</h1>
-        <CurrentIntro
-          location={weatherData.city}
-          time={new Date()}
-          details={{
-            temperature: weatherData.temperature,
-            temperatureFealing: weatherData.temperatureFealing,
-            conditions: weatherData.stateDescription,
-            icon: weatherData.stateIcon
-          }}
+      <CurrentIntro
+        location={weatherData.city}
+        time={new Date()}
+        details={{
+          temperature: weatherData.temperature,
+          temperatureFealing: weatherData.temperatureFealing,
+          conditions: weatherData.stateDescription,
+          icon: weatherData.stateIcon
+        }}
+      />
+      <div className={styles.detail}>
+        <div className={styles.detailsTitle}>
+          <TitleWithLine>Detalles</TitleWithLine>
+        </div>
+        <CurrentItem
+          value="Viento"
+          content={weatherData.windSpeed.toString()}
+          sub={weatherData.windAngle}
+          units="km/h"
+          icon="wind"
         />
-        <div className={styles.detail}>
-          <div className={styles.detailsTitle}>
-            <TitleWithLine>Detalles</TitleWithLine>
-          </div>
-          <CurrentItem
-            value="Viento"
-            content={weatherData.windSpeed.toString()}
-            sub={weatherData.windAngle}
-            units="km/h"
-            icon="wind"
-          />
-          <CurrentItem
-            value="Nubosidad"
-            content={weatherData.clouds.toString()}
-            units="%"
-            icon="cloud"
-          />
-          <CurrentItem
-            value="Humedad"
-            content={weatherData.humidity.toString()}
-            units="%"
-            icon="raindrop"
-          />
-          <CurrentItem
-            value="Presión"
-            content={weatherData.pressure.toString()}
-            units="mBar"
-            icon="pressure"
-          />
+        <CurrentItem
+          value="Nubosidad"
+          content={weatherData.clouds.toString()}
+          units="%"
+          icon="cloud"
+        />
+        <CurrentItem
+          value="Humedad"
+          content={weatherData.humidity.toString()}
+          units="%"
+          icon="raindrop"
+        />
+        <CurrentItem
+          value="Presión"
+          content={weatherData.pressure.toString()}
+          units="mBar"
+          icon="pressure"
+        />
+      </div>
+      <div className={styles.detail}>
+        <div className={styles.detailsTitle}>
+          <TitleWithLine>Salida/Puesta de sol</TitleWithLine>
         </div>
-        <div className={styles.detail}>
-          <div className={styles.detailsTitle}>
-            <TitleWithLine>Salida/Puesta de sol</TitleWithLine>
-          </div>
-          <CurrentItem
-            value="Salida sol"
-            content={weatherData.sunrise.toString()}
-            icon="sunrise"
-          />
-          <CurrentItem
-            value="Puesta sol"
-            content={weatherData.sunset.toString()}
-            icon="sunset"
-          />
-        </div>
+        <CurrentItem
+          value="Salida sol"
+          content={weatherData.sunrise.toString()}
+          icon="sunrise"
+        />
+        <CurrentItem
+          value="Puesta sol"
+          content={weatherData.sunset.toString()}
+          icon="sunset"
+        />
       </div>
     </section>
   );
